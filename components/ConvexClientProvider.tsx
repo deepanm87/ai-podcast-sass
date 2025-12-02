2a62ee2c98e94391395451e4a874ef0ab28373a6
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-12-01 20:21:24 -0500

    setup shadcn ui, inngest, clerk, convex config files

---
+++ components/ConvexClientProvider.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { ConvexProvider, ConvexReactClient } from "convex/react"
+import { ReactNode } from "react"
+import { useAuth } from "@clerk/nextjs"
+import { ConvexProviderWithClerk } from "convex/react-clerk"
+
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+  throw new Error("NEXT_PUBLIC_CONVEX_URL is not set")
+}
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string)
+
+export function ConvexClientProvider({ children }: { children: ReactNode }) {
+  return (
+    <ConvexProviderWithClerk
+      client={convex}
+      useAuth={useAuth}
+    >
+      {children}
+    </ConvexProviderWithClerk>
+  )
+}
